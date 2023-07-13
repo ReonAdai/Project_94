1bd9de9acb923866d45f2db7cf57962cf65e84c4
ReonAdai <113380691+ReonAdai@users.noreply.github.com>
2023-07-13 20:46:09 +0100

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyC-9jgJBU2jTkBNZhxYiypdhFRJ9KkHuwY",
+    authDomain: "letschat-web-app-11abb.firebaseapp.com",
+    projectId: "letschat-web-app-11abb",
+    storageBucket: "letschat-web-app-11abb.appspot.com",
+    messagingSenderId: "877987888394",
+    appId: "1:877987888394:web:717164248e371b6b848ef3"
+  };
+  
+firebase.initializeApp(firebaseConfig);
